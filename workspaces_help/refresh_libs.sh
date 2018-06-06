13cce0e9d69556a56fd42ee164394df5295969bd
Dana Sheahen <dsmsheahen@gmail.com>
2018-06-06 15:36:18 -0500

    wrap get in if statement

--- workspaces_help/refresh_libs.sh
+++ workspaces_help/refresh_libs.sh
@@ -1,1 +1,5 @@
-sudo apt-get install -y libglew-dev glew-utils libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libglib2.0-dev
+#!/bin/bash
+glewh = "/usr/include/GL/glew.h"
+if [-e "$glewh"] then
+  sudo apt-get install -y libglew-dev glew-utils libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libglib2.0-dev
+fi
